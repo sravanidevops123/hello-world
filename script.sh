a035e47b9830b4e67c8002e526f9eeff578926e3
sravani <110104308+sravanidevops123@users.noreply.github.com>
2022-08-08 12:51:32 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-git clone "https://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@github.com/sravanidevops123/hello-world.git"
+git clone "https://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@github.com/sravanidevops123/hello-world.git"
 cd hello-world
 git checkout master
 git tag ${tagname}
